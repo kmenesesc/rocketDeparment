b2291e086830acc03dc643c2a67784029b971f33
Emma <emma@rocketdept.com>
2015-06-01 22:35:24 -0700

    updated to csv logging, three activity levels

--- meowfit_logger.js
+++ meowfit_logger.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var intervalId;
 var connectedBean;
 var tempData = 0;
-var movement = false;
+var movement = 0;
 
 var lastX = 0;
 var lastY = 0;
@@ -16,34 +16,30 @@ Bean.discover(function(bean){
   bean.on("accell", function(x, y, z, valid){
     var status = valid ? "valid" : "invalid";
 
-    diffX = Math.abs(x - lastX);
-    diffY = Math.abs(y - lastY);
-    diffZ = Math.abs(z - lastZ);
+    diffX = Math.abs(x - lastX).toFixed(5);
+    diffY = Math.abs(y - lastY).toFixed(5);
+    diffZ = Math.abs(z - lastZ).toFixed(5);
 
     // check for movement
     if( diffX < 0.06 && diffY < 0.06 && diffZ < 0.06){
-      movement = false;
+      movement = 0;
+    } else if ( diffX > 0.8 || diffY > 0.8 || diffZ > 0.8) {
+      movement = 2;
     } else {
-      movement = true;
+      movement = 1;
     }
 
     var now = new Date();
     var jsonDate = now.toJSON();
 
-    var data = {
-      "datetime" : jsonDate,
-      "diffX" : diffX,
-      "diffY" : diffY,
-      "diffZ" : diffZ,
-      "movement" : movement,
-      "temp" : tempData
-    };
+    var data = jsonDate + ", " + diffX + ", " + diffY + ", " + diffZ + ", " + movement + ", " + tempData
+
 
     // output useful data to console
     console.log(data);
 
     //write to log file
-    fs.appendFile('log.txt', JSON.stringify(data, null, 4), function(err){
+    fs.appendFile('log.csv', data, function(err){
     })
 
     // update the last known values
