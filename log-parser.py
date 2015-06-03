e53e8e8d1d0ae25bb4949c81127187e8546a99e7
Emma <emma@rocketdept.com>
2015-06-03 00:43:09 -0700

    updated log parser for csv data, added pyplot figure

--- log-parser.py
+++ log-parser.py
@@ -1,21 +1,35 @@
 # collect and display data from log file
 
-dataPoints = 0
-movementTrue = 0
-movementFalse  = 0
+import matplotlib.pyplot as pyplot
+import csv
 
-with open("log-alpha.txt") as f:
-	for line in f:
-		if "movement" in line:
-			dataPoints = dataPoints + 1
-			
-			if "true" in line:
-				movementTrue = movementTrue + 1
-			else:
-				movementFalse = movementFalse + 1
+# dictionary to track data values
+data = {
+	' 0' : 0,
+	' 1' : 0,
+	' 2' : 0
+}
 
-print "Number Data Points: ", dataPoints
-print "Moving: ", movementTrue
-print "Resting: ", movementFalse
-print
-print "Percentage Moving: ", (float(movementTrue)/float(dataPoints)) * 100
+# open raw data file
+with open("output.txt") as f:
+	reader = csv.reader(f)
+	for line in reader:
+		# check for activity level and add to data dict
+		data[ line[4] ] = data[ line[4] ] + 1
+
+print data
+
+values = data.values()
+keys = [ "High Activity", "Resting", "Low Activty" ]
+colors = ['green', 'lightskyblue', 'yellowgreen']
+
+pyplot.axis("equal")
+pyplot.pie(
+        values,
+        labels=keys,
+        colors=colors,
+        autopct="%1.1f%%"
+        )
+pyplot.title("Chloe's Activity")
+pyplot.savefig("plot.png")
+pyplot.show()
